5df0909faa0217a4ab610f53fa88d29a817ab9e8
FlorinVasileNechita <nechitafv@gmail.com>
2017-01-30 02:23:07 +0200

    3

---
+++ app/src/main/java/com/example/usr/fragmentstest/BottomPictureFragment.java
@@ -0,0 +1,23 @@
+package com.example.usr.fragmentstest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by usr on 1/30/2017.
+ */
+
+public class BottomPictureFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.bottom_picture_fragment, container, false);
+        return view;
+    }
+}
